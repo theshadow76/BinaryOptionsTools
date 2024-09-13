fe8f74b3e00dba420480f0bbefa12f7df68c3453
vigo walker <vigopaul05@gmail.com>
2024-09-13 14:00:19 -0300

    fssfesf

--- BinaryOptionsTools/__init__.py
+++ BinaryOptionsTools/__init__.py
@@ -2,7 +2,7 @@
 
 # Pocket Option
 from BinaryOptionsTools.platforms.pocketoption.stable_api import PocketOption
-
+import time
 #--------------------- Pocket Option Wrapper ---------------------#
 class pocketoption:
     def __init__(self, ssid: str, demo: bool = True) -> None:
@@ -12,3 +12,8 @@ class pocketoption:
     def GetBalance(self):
         data = self.api.get_balance()
         return data
+    def Reconnect(self, retries: int = 1):
+        for i in range(1, retries):
+            self.api.connect()
+            print("Connecting...")
+            time.sleep(5)
