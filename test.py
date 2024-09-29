6b30bd145cabc09693edd775b0168aff043ec77f
vigo <vigopaul05@gmail.com>
2024-09-29 11:10:18 -0300

    dawdawd

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from BinaryOptionsTools import pocketoption
+from BinaryOptionsTools.indicators.momentum import rsi
 
 ssid = (r'42["auth",{"session":"vtftn12e6f5f5008moitsd6skl","isDemo":1,"uid":27658142,"platform":1}]')
 api = pocketoption(ssid, True)
 print(api.GetBalance())
+
+print(rsi(api, 60, "EURUSD_otc", 14))
