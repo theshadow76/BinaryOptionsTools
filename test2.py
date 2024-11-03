fb0290298edcd354b5710a7b85b788790c67c64a
vigo <vigopaul05@gmail.com>
2024-11-03 17:52:59 -0300

    few

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 from BinaryOptionsTools import pocketoption
+from BinaryOptionsTools.bot.signals import signals
+ssid = (r'42["auth",{"session":"vtftn12e6f5f5008moitsd6skl","isDemo":1,"uid":27658142,"platform":2}]')
+api = pocketoption(ssid, True)
 
-pocketoption("TEST_SSID", True)
-
-print(pocketoption.GetBalance())
+print(signals().sma_cross_over(api=api))
