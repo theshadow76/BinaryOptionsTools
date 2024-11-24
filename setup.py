235f6706981b35ac6400d4d5a6bdcbe1a350663a
vigo <vigopaul05@gmail.com>
2024-11-24 13:57:30 -0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='BinaryOptionstools',
-    version='0.1.0',
-    author='Vigo',
+    name='BinaryOptionsTools',
+    version='1.0.0',
+    author='Chipa',
     author_email='vigopaul05@gmail.com',
     description='Tools for Binary Options trading',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/BinaryOptionstools',
+    url='https://github.com/ChipaDevTeam/BinaryOptionsToolsV1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
